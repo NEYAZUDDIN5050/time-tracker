aec520f83aece513930e82d4e16b728780c5321b
neyazuddin5050 <neyazuddin5050@gmail.com>
2026-07-25 00:15:36 +0530

    kmlml

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
   return (
     <nav className="bg-slate-800 px-6 py-4 flex items-center justify-between">
       <h1 className="text-xl font-bold text-white">
-        Time<span className="text-indigo-400">Track</span>
+        Day<span className="text-indigo-400">Map</span>
       </h1>
       <Link to="/analytics" className="text-slate-700 hover:bg-blue-800 text-white text-sm px-4 py-2 rounded-lg transition">Analytics</Link>
      <Link to="/notes" className="text-slate-700 hover:bg-blue-800 text-white text-sm px-4 py-2 rounded-lg transition">Notes</Link>
