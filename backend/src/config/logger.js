2f720e6b1d49db03714c997d285434ebe09c040a
neyazuddin5050 <neyazuddin5050@gmail.com>
2026-07-24 23:33:51 +0530

    check 2

--- backend/src/config/logger.js
+++ backend/src/config/logger.js
@@ -1,15 +1,22 @@
 import winston from 'winston';
+import fs from 'fs';
+
+const logDir = 'logs';
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true });
+}
 
 const logger = winston.createLogger({
-    format: winston.format.combine(
+  level: 'info',
+  format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.json(),
-    ),
-    transports: [
+    winston.format.json()
+  ),
+  transports: [
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
-})
+});
 
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
