f35f1433ed6f67aca8e4633f8f814245466acc52
neyazuddin5050 <neyazuddin5050@gmail.com>
2026-07-24 23:45:41 +0530

    backend

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api',
-    header:{
-        'Content-Type': 'application/json',
-    },
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
 
-    const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    if(token){
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-})
+  return config;
+});
 
 export default axiosInstance;
